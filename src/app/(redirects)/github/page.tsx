2a825da51e48736140e843d0cab989b521291e57
Philipp Molitor <philipp@molitor-consulting.com>
2026-04-29 21:55:50 +0200

    fix url

--- src/app/(redirects)/github/page.tsx
+++ src/app/(redirects)/github/page.tsx
@@ -1,6 +1,8 @@
-import { GITHUB_URL } from "@/config";
+import { DOCS_GITHUB_OWNER } from "@/config";
 
 import { ClientRedirect } from "@/components/shared/ClientRedirect";
 
-const RedirectGithubPage = () => <ClientRedirect href={GITHUB_URL} />;
+const RedirectGithubPage = () => (
+  <ClientRedirect href={`https://github.com/${DOCS_GITHUB_OWNER}`} />
+);
 export default RedirectGithubPage;
